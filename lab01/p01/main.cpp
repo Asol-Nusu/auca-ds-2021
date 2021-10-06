aea27ea1a3bc55d000b434609eefea0ef606bdd6
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-10-06 18:59:00 +0200

    lab01 p01 part 3 completed

--- lab01/p01/main.cpp
+++ lab01/p01/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
-int main(){
+int main()
+{
+#ifdef AUCA_DEBUG
+    std::cout << "Author: Asol Nusuvalieva" << std::endl;
+#endif
+
     std::cout << "Hello, C++ " << __cplusplus << "!!!\n";
 }
