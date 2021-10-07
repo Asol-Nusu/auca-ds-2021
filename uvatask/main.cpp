17d8411de8297977f773819f56452e7e7dc891e0
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-10-07 12:46:13 +0200

    Add comments to clear up

--- uvatask/main.cpp
+++ uvatask/main.cpp
@@ -1,11 +1,24 @@
-#include <bits/stdc++.h> //include all the headers of C++ library  
-
+#include <bits/stdc++.h> 
+/*
+This header is a collection of all header files from C and C++ 
+standard libraries. 
+*/  
 template <typename C>
-int sz(const C &c) { return static_cast<int>(c.size()); }
+int sz(const C &c) { 
+    return static_cast<int>(c.size()); 
+    /*
+    c.size() return unsigned int which is bad. 
+    So that is why we are doing type conversion. 
+    */
+}
 
 using namespace std; //to avoid repetition
 
 int main()
 {
-    iostream::sync_with_stdio(false); //faster 
-}
+    iostream::sync_with_stdio(false); 
+    /*
+    Don't synchronize i/o C++ library with C library [only C++ i/o facilities].
+    Turn off synchronization with printf() and scanf()
+    */
+} 
