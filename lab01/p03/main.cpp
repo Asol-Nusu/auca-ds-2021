fba8010ae0c4191e9afc8fa1f36f31877452275d
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-10-07 15:06:14 +0200

    lab01 p03 completed

--- lab01/p03/main.cpp
+++ lab01/p03/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    string name;
-    int grade;
-
     //cin - character input
-    while (cin >> name >> grade) //cin ~ boolean
-    {
-        cout << name << " " << grade << "\n"; // cout - character output
+    //cout - character output
+    
+    const int kNameWidth = 30;
+    const int kAverageWidth = 40;
+    cout << setfill('.') << fixed;
+    for (string name; cin >> name;) //cin (attempt to read) ~ boolean
+    { 
+        double sum = 0;
+        int numberOfGrades = 0; 
+        for(int grade; cin >> grade;){
+            sum += grade;
+            ++numberOfGrades;
+        }
+        if(numberOfGrades != 0){
+            cout << left << setw(kNameWidth) << name << "|" << right << setw(kAverageWidth) << setprecision(2) << sum / numberOfGrades << "\n";
+        }else{
+            cout << left << setw(kNameWidth) << name << "|" << right << setw(kAverageWidth + 1) << "no data\n";
+        }
+        cin.clear(); 
     }
 }
 
