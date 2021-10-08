ac5c9d10fe64e09ba8d21908b5d3bb05282d87af
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-10-08 07:57:39 +0200

    UVA 1104 is not solved yet

---
+++ lab02/p03/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h> 
+
+template <typename C>
+int sz(const C &c) { 
+    return static_cast<int>(c.size()); 
+}
+
+using namespace std; 
+
+//11044
+int main()
+{
+    iostream::sync_with_stdio(false); 
+    
+    int testCases;
+    cin >> testCases;
+    for(int i = 0; i < testCases; ++i){
+        int rows;
+        int columns;
+
+        cin >> rows;
+        cin >> columns;
+        int grid[rows - 2][columns - 2];
+
+    }
+} 
+
+/*
+0 - not protected
+1 - protected
+8 - X
+*/
