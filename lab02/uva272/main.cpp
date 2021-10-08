53fae49f2274c9899b638f84471bd9271bcd2dd0
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-10-08 09:36:40 +0200

    Finish uva272

---
+++ lab02/uva272/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+  
+template <typename C>
+int sz(const C &c) { 
+    return static_cast<int>(c.size()); 
+}
+
+using namespace std; 
+
+int main(){
+    iostream::sync_with_stdio(false); 
+    int quoteCounter = 1; //odd - open, even - close 
+    for (char c; cin.get(c);) {
+        if(c == '"'){
+            string answer = (quoteCounter % 2 == 1) ? "``" : "''";
+            cout << answer;
+            quoteCounter++;
+        }else{
+            cout.put(c);
+        }
+    }
+} 
+
+/*
+``
+''
+*/
