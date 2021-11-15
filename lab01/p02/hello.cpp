3097ded4e6717499c4eb4c0d86f9e702be0b0593
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-11-15 09:28:31 +0100

    Add comment

--- lab01/p02/hello.cpp
+++ lab01/p02/hello.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "hello.hpp"
+#include "hello.hpp" //it's just a good idea since it can be useful with predicting potential errors
 
 void hello(){
     std::cout << "Hello, C++ " << __cplusplus << "!!!\n";
