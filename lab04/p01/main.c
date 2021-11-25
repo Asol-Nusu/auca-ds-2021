392d166a0d9d3ec09a081069020127e4e4829ef6
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-11-25 15:52:25 +0100

    Finish p01 and p02 in lab04

--- lab04/p01/main.c
+++ lab04/p01/main.c
@@ -2,6 +2,17 @@
 int main(void){
     int x;
     int y;
-    
-    printf("Hello, World!\n");
+    int *p;
+
+    x = 1;
+    p = &x;
+    printf("x = %d\n", x);
+    printf("x = %d\n", *p);
+    printf("address of x = %p\n", p); //hexadecimal value
+
+    *p = 2;
+    printf("x = %d\n", x);
+    printf("x = %d\n", *p);
+    y = *p * *p;
+    printf("y = %d\n", y);
 }
