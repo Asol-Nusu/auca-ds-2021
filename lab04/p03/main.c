a0dbd10cdf38a0a9545a92c4ef9301b12bc5083c
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-11-29 17:50:41 +0100

    Implement a new function

--- lab04/p03/main.c
+++ lab04/p03/main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+#define N 5
+#define M 3
+
+void printArray(int *p, int n){ //address of the 1st element
+    for(int i = 0; i < n; i++){
+        printf(" %d", p[i]);
+    }
+    printf("\n");
+}
+
 int main(void){
-    
+    int a[N];
+    int b[M];
+
+    for(int i = 0; i < N; i++){
+        scanf("%d", a + i);
+    }
+
+    for(int i = 0; i < M; i++){
+        scanf("%d", b + i);
+    }
+
+    printArray(a, N);
+    printArray(b, M);
 }
