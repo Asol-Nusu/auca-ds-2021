024fc1c014ce60d6af5f933fd1e936ffc5b4a3c9
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-11 19:59:01 +0100

    working on p2

--- project02/p02/main.cpp
+++ project02/p02/main.cpp
@@ -1,16 +1,61 @@
+/*
+ Each job description is terminated by a line containing a period.
+*/
 #include <bits/stdc++.h> 
+#include <string>
 
 template <typename C>
 int sz(const C &c) { 
     return static_cast<int>(c.size()); 
 }
 
-using namespace std; //to avoid repetition
+using namespace std;
+
+struct Skill{
+    string mName;
+    int mValue;
+
+    Skill(const string &name, double value)
+    : mName(name), mValue(value)
+    {
+    }
+
+    bool operator()() const{
+
+    }
+};
 
 int main()
 {
     iostream::sync_with_stdio(false); 
-    int r = -91;
-    int k = -30;
-    cout << r % 2 << "\n";
+
+    int nOfSkills;
+    int nOfJobDescriptions;
+    
+    cin >> nOfSkills >> nOfJobDescriptions;
+    vector<Skill> skills;
+
+    for(int i = 0; i < nOfSkills; i++){
+        string skillName;
+        int skillValue;
+        cin >> skillName >> skillValue;
+        skills.push_back(Skill(skillName, skillValue));
+    }
+    sort(begin(skills), end(skills), [](const Skill &skill1, const Skill &skill2){
+        return skill1.mName < skill2.mName;
+    });
+
+    int sum = 0;
+    for(int i = 0; i < nOfJobDescriptions; i++){
+        string word;
+        cin >> word;
+        while(word != "."){
+            
+            cin >> word;
+        }
+    }
+    /*
+    1) Sort a vector of skills based on names
+    2) lower_bound(begin(skills), end(skills), each_job_word)
+    */
 } 
