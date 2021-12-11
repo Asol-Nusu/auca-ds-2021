589fa20a45ae018e57d8e17d0cf82c5718362205
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-11 09:48:20 +0100

    Keep working on p01

--- project02/p01/main.cpp
+++ project02/p01/main.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h> 
+#include <algorithm>  
+#include <vector> 
 
 template <typename C>
 int sz(const C &c) { 
@@ -8,22 +10,41 @@ int sz(const C &c) {
 using namespace std;
 
 class MyFunctor{
-
+    int operator () (int num1, int num2) const {
+       return ;
+   }
 };
 
 //Отсортировать числа по возрастанию их остатков при делении на М
 int main()
 {
     iostream::sync_with_stdio(false); 
-    int numbers;
+    int n;
     int divider;
 
-    cin >> numbers >> divider;
-    while(numbers != 0 && divider != 0){
-        for(int i = 0; i < numbers; i++){
-            
+    cin >> n >> divider;
+    while(n != 0 && divider != 0){
+        vector<int> numbers = vector<int>(n);
+
+        for(int i = 0; i < n; i++){
+            int number;
+            cin >> number;
+            numbers.push_back(number);
         }
 
-        cin >> numbers >> divider;
+        sort(begin(numbers), end(numbers), [divider](int num1, int num2){
+            int result1 = num1 % divider;
+            int result2 = num2 % divider;
+
+            if(result1 != result2){
+                return result1 < result2;  //if r1 < r2 (true), num1 will go first            
+            }else{
+                if((num1 % 2 == 1 && num2 % 2 == 0) || (num2 % 2 == 1 && num1 % 2 == 0)){
+                    return 
+                }
+            }
+        });
+
+        cin >> n >> divider;
     }
 } 
