58cbf6fa92589b5ad3c9ea9e46b0cbedaa5d71c8
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-12 22:11:40 +0100

    Keep working on p06

--- project02/p06/main.cpp
+++ project02/p06/main.cpp
@@ -11,24 +11,61 @@ int sz(const C &c) {
 
 using namespace std; 
 struct Team{
+    friend bool operator== (const Team &lhs, const Team &rhs);
     string mName;
-    int rank; //to be calculated
+    int mRank; //to be calculated
 
-    int gamesPlayed;
+    int mGamesPlayed;
     
-    int wins;
-    int losses; 
-    int ties;
+    int mWins;
+    int mLosses; 
+    int mTies;
 
-    int goalsScored; 
-    int goalsAgainst;
-    int goalDifference;//их голы - чужие голы
+    int mGoalsScored; 
+    int mGoalsAgainst;
+    int mGoalDifference = mGoalsScored - mGoalsAgainst;//их голы - чужие голы //calculated inside
     
-    int totalPoints;
+    int mTotalPoints = mWins * 3 + mTies * 1; //calculated inside
     
     Team(string name)
         : mName(name)
     {
+    }
+};
+bool operator==(const Team &a, const Team &b){
+    return a.mName == b.mName;
+}
+
+struct CmpByName{
+    //Lexicographic order
+    bool operator()(const Team &t1, const Team &t2){
+        return t1.mName < t2.mName;
+    }
+};
+
+struct CmpByResult{
+    bool operator()(const Team &t1, const Team &t2){
+        if(t1.mTotalPoints != t2.mTotalPoints){
+            return t1.mTotalPoints > t2.mTotalPoints;
+        }else{
+            if(t1.mWins != t2.mWins){
+                return t1.mWins > t2.mWins;
+            }else{
+                if(t1.mGoalDifference != t2.mGoalDifference){
+                    return t1.mGoalDifference > t2.mGoalDifference;
+                }else{
+                    if(t1.mGoalsScored != t2.mGoalsScored){
+                        return t1.mGoalsScored > t2.mGoalsScored;
+                    }else{
+                        if(t1.mGamesPlayed != t2.mGamesPlayed){
+                            return t1.mGamesPlayed < t2.mGamesPlayed;
+                        }else{
+                            CmpByName();
+                        }
+                    }
+                }
+            }
+        }
     }
 };
 
@@ -57,7 +94,70 @@ int main()
         team_name_1#goals1@goals2#team_name_2
         */
         for(int game = 0; game < nOfGamesPlayed; game++){
-            
+            string input;
+            getline(cin, input);
+
+            //scannign team1Name
+            string team1Name;
+            int index = 0;
+            while(input.at(index) != '#'){
+                team1Name.push_back(input.at(index));
+                index++;
+            }
+
+            //scanning team1goals
+            index++;
+            string s1Goals;
+            while(input.at(index) != '@'){
+                s1Goals.push_back(input.at(index));
+                index++;
+            }
+            int team1Goals = stoi(s1Goals);
+
+            //scanning team2goals
+            index++;
+            string s2Goals;
+            while(input.at(index) != '#'){
+                s2Goals.push_back(input.at(index));
+                index++;
+            }
+            int team2Goals = stoi(s2Goals);
+
+            //scanning team2Name
+            index++;
+            string team2Name;
+            while(index != sz(input)){
+                team2Name.push_back(input.at(index));
+                index++;
+            }
+
+            //Working with formed teams
+            //It's guaranteed it'll find the team
+            auto team1 = find(begin(teams), end(teams), Team(team1Name));
+            (*team1).mGamesPlayed++;
+            (*team1).mGoalsScored += team1Goals;
+            (*team1).mGoalsAgainst += team2Goals;
+
+            auto team2 = find(begin(teams), end(teams), Team(team2Name));
+            (*team2).mGamesPlayed++;
+            (*team2).mGoalsScored += team2Goals;
+            (*team2).mGoalsAgainst += team1Goals;
+
+            if(team1Goals > team2Goals){
+                (*team1).mWins++;
+                (*team2).mLosses++;
+            }else if(team1Goals < team2Goals){
+                (*team2).mWins++;
+                (*team1).mLosses++;
+            }else{
+                (*team1).mTies++;
+                (*team2).mTies++;
+            }
         }
+
+        //Ranking (sorting)
+        sort(begin(teams), end(teams), CmpByResult());
+
+        //Printing the final result
     }
 } 
