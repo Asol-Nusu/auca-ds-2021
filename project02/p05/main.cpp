1a4950c83cb8468f6d5a28ced3df38a94f1dea50
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-12 20:06:26 +0100

    Accepted

--- project02/p05/main.cpp
+++ project02/p05/main.cpp
@@ -1,6 +1,6 @@
+//ACCEPTED
 /*
 Find out the frequencies of the ASCII characters.
-
 */
 #include <bits/stdc++.h> 
  
@@ -29,38 +29,38 @@ bool operator==(const Pair &a, const Pair &b){
 int main()
 {
     iostream::sync_with_stdio(false); 
-    vector<Pair> pairs;
 
+    int presentationChecker = 0;
     string input;
     while(getline(cin, input)){
+        if(presentationChecker != 0){
+            cout << "\n";
+        }
+
         //Working on a string input
-        cout << "the input is " << input << "\n";
-        for(int i = 0; i < (int)input.length(); i++){
-            char character = input.at(i);
-           
-            auto isExistingElement = find(begin(pairs), end(pairs), Pair(character));
+        vector<Pair> pairs;
+        for(auto c : input){
+            auto isExistingElement = find(begin(pairs), end(pairs), Pair(c));
             if(isExistingElement != end(pairs)){ //if found
                 (*isExistingElement).mOccurrence++;
             }else{
-                pairs.push_back(Pair(character));
+                pairs.push_back(Pair(c));
             }
-
-            //sorting 
-            sort(begin(pairs), end(pairs), [](const Pair &a, const Pair &b){
-                if(a.mASCIIValue != b.mASCIIValue){
-                    return a.mOccurrence < b.mOccurrence;
-                }else{
-                    return a.mASCIIValue > b.mASCIIValue;
-                }
-            });
-
-            //printing 
-            for(int j = 0; j < sz(pairs); j++){
-                cout << pairs[i].mASCIIValue << " " << pairs[i].mOccurrence << "\n";
-            }
-
-            cout << "\n";
         }
-    }
 
+        //sorting 
+        sort(begin(pairs), end(pairs), [](const Pair &a, const Pair &b){
+            if(a.mOccurrence != b.mOccurrence){
+                return a.mOccurrence < b.mOccurrence;
+            }else{
+                return a.mASCIIValue > b.mASCIIValue;
+            }
+        });
+
+        //printing 
+        for(int j = 0; j < sz(pairs); j++){
+            cout << pairs[j].mASCIIValue << " " << pairs[j].mOccurrence << "\n";
+        }
+        presentationChecker = 1;
+    }
 } 
