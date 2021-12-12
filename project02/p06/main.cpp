d6b365326bec1a470360ee1207a2845d4d7f3533
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-12 20:29:53 +0100

    Keep working on p06

--- project02/p06/main.cpp
+++ project02/p06/main.cpp
@@ -10,9 +10,28 @@ int sz(const C &c) {
 }
 
 using namespace std; 
+struct Team{
+
+};
 
 int main()
 {
     iostream::sync_with_stdio(false); 
-    
+    int nOfTournaments;
+    cin >> nOfTournaments;
+    for(int tour = 0; tour < nOfTournaments; tour++){
+        //Tournament Description
+        string tournamentName; //100 is max 
+        int nOfTeams;
+
+        for(int team = 0; team < nOfTeams; team++){
+            string teamName; //no ‘#’ and ‘@’, 30 is max
+        }
+
+        int nOfGamesPlayed;
+        //game results
+        for(int game = 0; game < nOfGamesPlayed; game++){
+            
+        }
+    }
 } 
