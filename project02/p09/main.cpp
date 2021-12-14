8ab3433d0908c563164a35c70247b1955675379e
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-14 16:59:47 +0100

    Keep working

--- project02/p09/main.cpp
+++ project02/p09/main.cpp
@@ -5,6 +5,10 @@ build the longest loop of rope while alternating colors.
 - Alternate between colors
 - If only one color, output 0
 - --*-- (knot consumes 0.5 cm from each rope)
+
+accumulate() 
+The result of accumulating 
+init and all the elements in the range [first,last).
 */
 #include <bits/stdc++.h> 
 #include <string>
@@ -16,20 +20,20 @@ int sz(const C &c) {
 
 using namespace std; 
 struct RopeSegment{
-    int length;
-    char color;
+    int mLength;
+    char mColor;
 
     RopeSegment(const string &rawInput)
-        : color(rawInput.back())
+        : mColor(rawInput.back())
     {
         //1 ≤ 𝐿 ≤ 100 and colors is R or B
         string sLength;
         for(auto c : rawInput){
-            if(c != color){
+            if(c != mColor){
                 sLength.push_back(c);
             }
         }
-        length = stoi(sLength);
+        mLength = stoi(sLength);
     }
 };
 
@@ -51,7 +55,15 @@ int main()
             ropes.push_back(RopeSegment(rawInput));
         }
 
+        //Sort ropes based on lenght (from max to min)
+        sort(begin(ropes), end(ropes), [](const RopeSegment &r1, const RopeSegment &r2)
+        {
+            return r1.mLength > r2.mLength;
+        });
 
+        RopeSegment resultingRope = accumulate(begin(ropes), end(ropes), 0, [](const RopeSegment &baseRope, const RopeSegment &ropeSegment){
+
+        });
 
         cout << "Case #" << test << ": " << "\n";
     }
