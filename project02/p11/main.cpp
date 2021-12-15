7e0c9c20edc39bed782290a3d035b18475464745
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-15 08:35:10 +0100

    Keep Working

--- project02/p11/main.cpp
+++ project02/p11/main.cpp
@@ -7,8 +7,28 @@ int sz(const C &c) {
 
 using namespace std; 
 
+//use stable_sort with lambda expression
 int main()
 {
     iostream::sync_with_stdio(false); 
-    
+    int nOfWords;
+    int wordLength;
+
+    bool firstTime = true;
+    cin >> nOfWords >> wordLength;
+    while(nOfWords != 0 && wordLength != 0){
+        if(!firstTime){
+            cout << "\n";
+        }
+
+        //receiving each word, I sort Column Strings
+        for(int i = 0; i < nOfWords; i++){
+            string word;
+            cin >> word;
+            
+        }
+
+        firstTime = false;
+        cin >> nOfWords >> wordLength;
+    }
 } 
