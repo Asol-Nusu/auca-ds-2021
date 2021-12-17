43aaba60743dcb3f34b802d35c84660bf66a6951
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-17 09:35:43 +0100

    Add a method

--- project02/p12/main.cpp
+++ project02/p12/main.cpp
@@ -2,7 +2,7 @@
 sort a group of people by their classes 
 to show their true place in the social class hierarchy.
 
-three main classes: upper, middle, and lower.
+three main classes: upper (2), middle (1), and lower (0).
 upper-upper > middle-upper > lower-upper
 
 all of the upper classes (upper-upper, middle-upper, and lower-upper) 
@@ -10,6 +10,12 @@ are higher than any of the middle classes.
 
 When comparing classes, once you have reached the lowest level of detail, 
 you should assume that all further classes are the same as the middle level of the previous level of detail.
+
+middle-middle-lower-middle == lower-middle
+1-1-0-1 == 0-1
+
+upper == middle-upper
+1-2 == 2
 */
 #include <bits/stdc++.h> 
 
@@ -19,23 +25,37 @@ int sz(const C &c) {
 }
 
 using namespace std; 
-pair<string, string> produceValidValues(const string &rawInput){
+char convertToSocialClass(const string &sSocialClass){
+    if(sSocialClass == "upper"){
+        return '0';
+    }else if(sSocialClass == "middle"){
+        return '1';
+    }else{
+        return '2';
+    }
+}
+pair<string, string> produceValidValues(string rawInput){
+    replace(rawInput.begin(), rawInput.end(), ':', ' ');
+    replace(rawInput.begin(), rawInput.end(), '-', ' ');
+
+    istringstream streamInput(rawInput); //Input stream class to operate on strings
+    
     string name;
     string socialClass;
-    bool isSocialClassPart = false;
-    for(auto c : rawInput){
-        if(c != ':' && c != ' ' && !isSocialClassPart){
-            name.push_back(c);
-        }else if(c == ':' || c == ' '){
-            isSocialClassPart = true;
+    int index = 0; 
+    for(string word; streamInput >> word;){
+        if(word == "class"){
             continue;
         }
-
-        if(isSocialClassPart){
-            socialClass.push_back(c);
+        if(index == 0){
+            name = word;
+        }else{
+            char intSocialClass = convertToSocialClass(word);
+            socialClass.push_back(intSocialClass);
         }
+        
+        index++;
     }
-
     return make_pair(name, socialClass);
 }
 
@@ -60,9 +80,10 @@ int main()
         for(int j = 0; j < nOfPeople; j++){
             string rawInput;
             getline(cin, rawInput);
+
             pair<string, string> person = produceValidValues(rawInput);
             cout << person.first << " " << person.second << "\n";
         }
-        outputEqualSigns();
+        //outputEqualSigns();
     }
 } 
