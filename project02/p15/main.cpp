372c00e843ee83eff62f3cfbb2bb9813aeee46f9
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-25 17:51:15 +0100

    Keep working on p15

--- project02/p15/main.cpp
+++ project02/p15/main.cpp
@@ -1,3 +1,10 @@
+/*
+The first sort starts from the order of songs as given in the input. 
+Each subsequent sort starts from the order given by the previous sort, 
+rather than starting from the initial order. 
+All sorting should be done least to greatest, in lexicographic order 
+(using ASCII values, even for numeric data).
+*/
 #include <bits/stdc++.h> 
 
 template <typename C>
@@ -7,9 +14,29 @@ int sz(const C &c) {
 
 using namespace std; 
 
+//stable_sort with lambda expression (capture), find
 int main()
 {
     iostream::sync_with_stdio(false); 
-    string asol = "Asol";
-    cout << asol.substr(0, 2); //pos, length
+
+    //Attributes
+    string attributesString;
+    getline(cin, attributesString);
+    istringstream streamInput(attributesString);
+    
+    //Splitting attributes 
+    vector<string> attributes;
+    for(string attribute; streamInput >> attribute;){
+        attributes.push_back(attribute);
+    }
+
+    int nOfSongs;
+    cin >> nOfSongs;
+    cin.ignore(10000, '\n');
+    
+    vector<vector<string>> playList;
+    for(int i = 0; i < nOfSongs; i++){
+        string input;
+        getline()
+    }
 } 
