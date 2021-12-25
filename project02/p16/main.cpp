4fae23fc340091da8f9a599f78f069af28ded758
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-25 20:05:33 +0100

    keep solving p16

--- project02/p16/main.cpp
+++ project02/p16/main.cpp
@@ -12,8 +12,21 @@ enum class Event{
 };
 
 //sort, pair<int, Event>, where Event: enum class Event {Departure, Arrival}
+//2013-2016
 int main()
 {
     iostream::sync_with_stdio(false); 
-    
+    int tests;
+    cin >> tests;
+    for(int test = 0; test < tests; test++){
+        int nOfBookings;
+        int roomCleaningTime;
+        cin >> nOfBookings >> roomCleaningTime; //in mins
+
+        for(int i = 0; i < nOfBookings; i++){
+            string reservationCode;
+            
+        }
+
+    }
 } 
