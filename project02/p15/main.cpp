6d23fcd0468ce7c8ff73cd11f7c0769753c5c255
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-25 19:00:14 +0100

    logical error

--- project02/p15/main.cpp
+++ project02/p15/main.cpp
@@ -13,18 +13,27 @@ int sz(const C &c) {
 }
 
 using namespace std; 
-vector<pair<int, string>> convert(const string &rawInput){
+vector<pair<int, string>> convertToPairs(const string &rawInput){
     istringstream streamInput(rawInput);
 
     vector<pair<int, string>> pairs;
 
     int index = 0;
     for(string s; streamInput >> s;){
-        strings.push_back(make_pair(index, s));
+        pairs.push_back(make_pair(index, s));
         index++;
     }
 
     return pairs;
+}
+
+vector<string> convertToStrings(const string &rawInput){
+    istringstream streamInput(rawInput);
+    vector<string> strings;
+    for(string s; streamInput >> s;){
+        strings.push_back(s);
+    }
+    return strings;
 }
 //stable_sort with lambda expression (capture), find
 int main()
@@ -35,26 +44,54 @@ int main()
     string attributesString;
     getline(cin, attributesString);
     //Splitting attributes 
-    vector<pair<int, string>> attributes = convert(attributesString); //each attribute will have its index
+    vector<pair<int, string>> attributes = convertToPairs(attributesString); //each attribute will have its index
 
     int nOfSongs;
     cin >> nOfSongs;
     cin.ignore(10000, '\n');
 
-    vector<vector<pair<int, string>>> playList;
-    /*vector of songs; each song is a vector of pairs<int, string>*/
+    vector<vector<string>> playList;
+    /*vector of songs; each song is a vector of separated string infos*/
     for(int i = 0; i < nOfSongs; i++){
         string record;
         getline(cin, record);
-        vector<pair<int, string>> recordInfo = convert(record);
+        vector<string> recordInfo = convertToStrings(record);
         playList.push_back(recordInfo);
     }
     
     int nOfCommands;
     cin >> nOfCommands;
+    bool firstTime = true;
     for(int i = 0; i < nOfCommands; i++){
+        if(!firstTime){
+            cout << endl;
+        }
         string command;
         cin >> command;
         
+        auto foundAttribute = find_if(begin(attributes), end(attributes), [&command](const pair<int, string> &attribute){
+            return attribute.second == command;
+        });
+
+        int commandIndex;
+        if(foundAttribute != end(attributes)){
+            commandIndex = (*foundAttribute).first;
+        }
+
+        stable_sort(begin(playList), end(playList), [&commandIndex](const vector<string> &record1, const vector<string> &record2)
+        {
+            return record1[commandIndex] < record2[commandIndex];
+        });
+
+        cout << attributesString << endl;
+        for(int record = 0; record < (int)playList.size(); i++){
+            for(int recordInfo = 0; recordInfo < (int)playList[record].size(); recordInfo++){
+                bool finish = recordInfo == ((int)playList[record].size() - 1);
+                string r = (!finish) ? " " : "\n";
+                cout << playList[record][recordInfo] << r;
+            }
+        }
+
+        firstTime = false;
     }
 } 
