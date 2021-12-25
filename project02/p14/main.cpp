642158b76dba97e92283fcb7857b6fb86092b444
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-25 17:18:20 +0100

    Start solving p14

--- project02/p14/main.cpp
+++ project02/p14/main.cpp
@@ -1,3 +1,11 @@
+/*
+sorts a list of student last names, 
+but the sort only uses the first two letters of the name. 
+Nothing else in the name is used for sorting. 
+
+However, if two names have the same first two letters, 
+they should stay in the same order as in the input
+*/
 #include <bits/stdc++.h> 
 
 template <typename C>
@@ -7,19 +15,35 @@ int sz(const C &c) {
 
 using namespace std; 
 
+//stable_sort with lambda expression
 int main()
 {
     iostream::sync_with_stdio(false); 
-    vector<string> words = {"apple",  "banaca", "bananba", "kiwi", "pear", "zzzza"};
-    sort(begin(words), end(words), [](const string &w1, const string &w2){
-        string reversedW1 = w1;
-                string reversedW2 = w2;
-                reverse(reversedW1.begin(), reversedW1.end());
-                reverse(reversedW2.begin(), reversedW2.end());
+    bool firstTime = true;
+    int nOfPeople;
+    while(cin >> nOfPeople && nOfPeople != 0){
+        if(!firstTime){
+            cout << endl;
+        }
+        vector<string> names;
+        string name;
+        for(int i = 0; i < nOfPeople; i++){
+            getline(cin, name);
+            names.push_back(name);
+            cout << "Just read the input: " << name << endl;
+        }
 
-                return reversedW1 < reversedW2;
-    });
-    for(string w : words){
-        cout << " " << w;
-    }
+        stable_sort(begin(names), end(names), [](const string &name1, const string &name2)
+        {
+            string name1Part = name1.substr(0, 2); //pos, length
+            string name2Part = name2.substr(0, 2); //pos, length
+            return name1Part < name2Part;
+        });
+
+        //printing 
+        for(auto n : names){
+            cout << name << endl;
+        }
+        firstTime = false;
+    }    
 } 
