e13f91ed14919f49b6c855e40143b14d93f9a5b9
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-27 12:11:39 +0100

    FINALLY ACCEPTED

--- project02/p07/main.cpp
+++ project02/p07/main.cpp
@@ -1,3 +1,4 @@
+//ACCEPTED
 /*
 Contestants = 100 (max)
 Problems = 9 (max)
@@ -9,36 +10,7 @@ int sz(const C &c) {
     return static_cast<int>(c.size()); 
 }
 
-using namespace std; 
-
-struct Problem{
-    int mNumber;
-    char mStatus = ' ';
-
-    int mNOfSubmissions = 0;
-    bool mWasPreviouslyIncorrect = false;
-
-    int mPenaltyTime = 0;
-    
-
-    Problem(const int &problemNumber)
-        : mNumber(problemNumber) 
-    {
-    }
-};
-
-struct Contestant{
-    int mName;
-    vector<Problem> problems; //correct с первого раза + the ones which got eventually correct
-    int mTotalSolvedProblems = 0;
-    int mTotalPenaltyTime = 0;
-
-    Contestant(const int &name)
-        : mName(name)
-    {
-    }
-    
-};
+using namespace std;
 
 struct Submission{
     int mContestant;
@@ -50,11 +22,43 @@ struct Submission{
         istringstream streamInput(input);
         streamInput >> mContestant >> mProblem >> mPenaltyTime >> mProblemStatus;
     }
-}
+};
+
+using Problem = pair<bool, int>;
+//solved and nOfAttempts
+
+struct Contestant{
+    int mNumber;
+    int mTotalSolvedProblems;
+    int mTotalPenaltyTime;
+    bool mHadAtLeastOneSubmission;
+    vector<Problem> mProblems;
+
+    Contestant(int number)
+        : mNumber(number), mTotalSolvedProblems(0), mTotalPenaltyTime(0), mHadAtLeastOneSubmission(false), mProblems(9)
+    {
+    }
+
+    void processSubmission(const Submission submission){
+        mHadAtLeastOneSubmission = true;
+        if(mProblems[submission.mProblem - 1].first){
+            return;
+        }
+
+        if(submission.mProblemStatus == 'C'){
+            mTotalSolvedProblems++;
+            mProblems[submission.mProblem - 1].first = true;
+            mTotalPenaltyTime += submission.mPenaltyTime + mProblems[submission.mProblem - 1].second;
+
+        }else if(submission.mProblemStatus == 'I' ){
+            mProblems[submission.mProblem - 1].second += 20;
+        }
+    } 
+};
 
 struct CmpByTeamNumber{
     bool operator()(const Contestant &c1, const Contestant &c2){
-        return c1.mName < c2.mName;
+        return c1.mNumber < c2.mNumber;
     }
 };
 struct CmpByACMRules{
@@ -80,13 +84,30 @@ int main()
     iostream::sync_with_stdio(false); 
 
     int tests;
-    cin >> tests;
+    cin >> tests >> ws;
+
+    
     for(int test = 0; test < tests; test++){
         vector<Contestant> contestants;
-        
+        for(int i = 1; i <= 100; i++){
+            contestants.emplace_back(i);
+        }
+
         //Submissions
         for(string input; getline(cin, input) && !input.empty();){
             Submission submission(input);
-
+            contestants[submission.mContestant - 1].processSubmission(submission);
         } 
+
+        sort(begin(contestants), end(contestants), CmpByACMRules());
+
+        if(test != 0){
+            cout << "\n";
+        }
+        for(auto &contestant : contestants){
+            if(contestant.mHadAtLeastOneSubmission){
+                cout << contestant.mNumber << " " << contestant.mTotalSolvedProblems << " " << contestant.mTotalPenaltyTime << "\n";
+            }
+        }
+    }
 } 
