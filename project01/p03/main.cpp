5a79482d3b008dfd94e2a3a00cf50e18a62433df
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-27 22:04:14 +0100

    Start p03

---
+++ project01/p03/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+#include "../BigInt/BigInt.hpp"
+
+template <typename C>
+int sz(const C &c) { 
+    return static_cast<int>(c.size()); 
+}
+
+using namespace std; 
+
+int main()
+{
+    iostream::sync_with_stdio(false); 
+    int fNumber;
+    while(cin >> fNumber){
+        
+    }
+} 
