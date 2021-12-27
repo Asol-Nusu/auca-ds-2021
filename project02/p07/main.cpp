63cb7a8c9736151dbb41a53abd8b448d51ff6755
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-27 11:09:38 +0100

    Chnage the whole logic

--- project02/p07/main.cpp
+++ project02/p07/main.cpp
@@ -40,6 +40,18 @@ struct Contestant{
     
 };
 
+struct Submission{
+    int mContestant;
+    int mProblem;
+    int mPenaltyTime;
+    char mProblemStatus;
+
+    Submission(const string &input){
+        istringstream streamInput(input);
+        streamInput >> mContestant >> mProblem >> mPenaltyTime >> mProblemStatus;
+    }
+}
+
 struct CmpByTeamNumber{
     bool operator()(const Contestant &c1, const Contestant &c2){
         return c1.mName < c2.mName;
@@ -61,123 +73,20 @@ struct CmpByACMRules{
    }
 };
 
-vector<string> makeGoodValues(const string &input){
-    vector<string> result;
-    istringstream ss(input);
 
-    string word; // for storing each word
-    while (ss >> word) 
-    {
-        result.push_back(word);
-    }
-
-    return result;
-}
 //use find_if, sort
 int main()
 {
     iostream::sync_with_stdio(false); 
+
     int tests;
     cin >> tests;
-    cin.ignore(10000, '\n');
-    string trash;
-    getline(cin, trash); //FOR CORRECT READING
-
     for(int test = 0; test < tests; test++){
         vector<Contestant> contestants;
         
         //Submissions
-        string input;
-        while(getline(cin, input) && !input.empty()){
-            vector<string> goodResult = makeGoodValues(input);
-            int contestantName = stoi(goodResult[0]);
-            int problemNumber = stoi(goodResult[1]);
-            int penaltyTime = stoi(goodResult[2]);
-            char problemStatus = goodResult[3].at(0);
+        for(string input; getline(cin, input) && !input.empty();){
+            Submission submission(input);
 
-            auto isExistingContestant = find_if(begin(contestants), end(contestants), [contestantName](const Contestant &contestant){
-                return contestant.mName == contestantName;
-            });
-
-            /*if person exists*/if(isExistingContestant != end(contestants)){
-                auto didWorkOnThisProblem = find_if(begin((*isExistingContestant).problems), end((*isExistingContestant).problems), [problemNumber](const Problem &problem){
-                        return problemNumber == problem.mNumber;
-                    });
-
-                if(didWorkOnThisProblem != end((*isExistingContestant).problems)){
-                    if(problemStatus == 'C'){
-                        if((*didWorkOnThisProblem).mStatus == 'R' || (*didWorkOnThisProblem).mStatus == 'U' || (*didWorkOnThisProblem).mStatus == 'E' || (*didWorkOnThisProblem).mStatus == ' '){
-                            (*didWorkOnThisProblem).mStatus = 'C';
-                            (*didWorkOnThisProblem).mPenaltyTime = penaltyTime;
-                        }else if((*didWorkOnThisProblem).mStatus == 'I'){
-                            (*didWorkOnThisProblem).mWasPreviouslyIncorrect = true;
-                            (*didWorkOnThisProblem).mStatus = 'C';
-                            (*didWorkOnThisProblem).mPenaltyTime = penaltyTime;
-                        }
-                    }else if(problemStatus == 'I'){
-                        (*didWorkOnThisProblem).mNOfSubmissions++;
-                    }
-
-
-                }else{
-                    (*isExistingContestant).problems.push_back(Problem(problemNumber));
-                    if(problemStatus == 'C'){
-                        (*isExistingContestant).problems.back().mStatus = 'C';
-                        (*isExistingContestant).problems.back().mPenaltyTime = penaltyTime;
-                    }else if(problemStatus == 'I'){
-                        (*isExistingContestant).problems.back().mStatus = 'I';
-                        (*isExistingContestant).problems.back().mNOfSubmissions++;
-                    }
-                } 
-
-            /* if doesn't person exist*/}else{
-                contestants.push_back(Contestant(contestantName));
-                if(problemStatus == 'C'){
-                    contestants.back().problems.push_back(Problem(problemNumber));
-                    contestants.back().problems.back().mStatus = 'C';
-                    contestants.back().problems.back().mPenaltyTime = penaltyTime;
-                }else if(problemStatus == 'I'){
-                    contestants.back().problems.push_back(Problem(problemNumber));
-                    contestants.back().problems.back().mStatus = 'I';
-                    contestants.back().problems.back().mNOfSubmissions++;
-                }else{
-                    //U, E, R
-                    contestants.back().problems.push_back(Problem(problemNumber));
-                    contestants.back().problems.back().mStatus = problemStatus;
-                }
-            }
-        }
-
-        //Calculate Standings
-        for(auto contestant : contestants){
-            int contestantSolvedPs = 0;
-            int contestantPenaltyTime = 0;
-            for(auto problem : contestant.problems){
-                if(problem.mStatus == 'C'){
-                    contestantSolvedPs++;
-                    contestantPenaltyTime += problem.mPenaltyTime;
-
-                    if(problem.mWasPreviouslyIncorrect){
-                        contestantPenaltyTime += 20*problem.mNOfSubmissions;
-                    }
-                }
-            }
-            contestant.mTotalSolvedProblems = contestantSolvedPs;
-            contestant.mTotalPenaltyTime = contestantPenaltyTime;
-            cout << "Total Solved problems: " << contestant.mTotalSolvedProblems << "\n";
-            cout << "Total penalty time: " << contestant.mTotalPenaltyTime << "\n";
-        }
-        //Sorting contestants
-        sort(begin(contestants), end(contestants), CmpByACMRules());
-
-        //printing
-        for(auto contestant : contestants){
-            cout << contestant.mName << " " << contestant.mTotalSolvedProblems << " " << contestant.mTotalPenaltyTime << "\n";
-        }
-
-        //To avoid Presentation Error
-        if(test != tests - 1){
-            cout << "\n";
-        }
-    } 
+        } 
 } 
