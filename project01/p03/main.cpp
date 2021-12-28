57b460edc2a3b9f3257f40cc929aac5a42f4c6d9
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-12-28 08:55:10 +0100

    Accepted*

--- project01/p03/main.cpp
+++ project01/p03/main.cpp
@@ -1,3 +1,4 @@
+//ACCEPTED*
 #include <bits/stdc++.h> 
 #include "../BigInt/BigInt.hpp"
 
@@ -8,28 +9,10 @@ int sz(const C &c) {
 
 using namespace std; 
 
-BigInt produceFibonacciNumber(int fNumber){
-    if(fNumber == 0){
-        return BigInt(0);
-    }else if(fNumber == 1){
-        return BigInt(1);
-    }
-    
-    vector<BigInt> fibonacciNumbers;
-    fibonacciNumbers.push_back(0);
-    fibonacciNumbers.push_back(1);
-
-    for(int i = 2; i <= fNumber; i++){
-        BigInt currentNum = fibonacciNumbers[i-1] + fibonacciNumbers[i-2];
-        fibonacciNumbers.push_back(currentNum);
-    }
-    
-    return fibonacciNumbers[fNumber];
-}
 int main()
 {
     iostream::sync_with_stdio(false); 
-    
+
     vector<BigInt> fibonacciNumbers;
     fibonacciNumbers.push_back(0);
     fibonacciNumbers.push_back(1);
